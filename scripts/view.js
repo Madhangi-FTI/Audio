9bd67322864eb60dd5c41364c0f79bd8f3402aea
Maddy Ramasamy <madhangi.ramasamy@fticonsulting.com>
2018-09-05 00:52:36 -0400

    Update view.js

--- scripts/view.js
+++ scripts/view.js
@@ -10,12 +10,11 @@ function SetupEventHandlers() {
     curHiLiteId = $(), curElement = $(),
         ccTable = $("#ccTable"), redTable = $("#redTable"), fixTable = $("#fixTable"),
         player = $("#jp_player");
-
     setupWindowScroll();
     setupRedactionSetButtons();
 }
 
-function formatTimeForDisplay(time) {
+function formatTimeForDisplay (time) {
     var totalSec = new Date(parseInt(time) * 1000) / 1000;
     var hours = parseInt(totalSec / 3600) % 24;
     var minutes = parseInt(totalSec / 60) % 60;
@@ -32,58 +31,53 @@ Element.prototype.HiLiteTranscript = function ( locId) {
     $(this).addClass("hiLite");
     curHiLiteId = $(this);
     
+	pageTop = 0;
+	
     var element = this.parentNode.parentNode;
-    if (element != curElement)
-    {
-        var overTop = element.offsetTop - tranDiv.scrollTop >= tranHeight - 6 /*padding...*/,
-            overBottom = (tranDiv.scrollTop - tranHeight) > (element.offsetTop - (tranHeight - element.offsetTop - element.clientHeight));
-        
-	    if (((element.offsetTop + (element.clientHeight + 20)) > (tranHeight + tranDiv.children[0].scrollTop)-20) ) {
-	        element.scrollIntoView( true); 
-
-	   }
-       // if (overTop || overBottom) element.scrollIntoView( true); 
+    if (element != curElement) {
+		element.scrollIntoView( {behavior: "smooth", block: "center", inline: "nearest"});
         curElement = element;
     }
     return false;
 }
 
 function organizeDisplay() {
-    $("#tabs-a").tabs();
-    $("#tabs-b").tabs();
-
-    document.body.style.overflow = "hidden";
-    tranDiv = $("#tabs-transcript")[0],
-    tranStyle = window.getComputedStyle(tranDiv, null),
-    tranBorder = parseInt(tranStyle.getPropertyValue('border-top-width')),
-    tranOffset = tranDiv.offsetTop,
-    //tranHeight = $(window).height() - tranDiv.offsetTop - 10;
-    tranHeight;
-    //$("#tabs-transcript").height(tranHeight);
-    document.body.style.overflow = "";
+    $("#tabs-a" ).tabs();
+    $("#tabs-b" ).tabs();
+    
+    tranDiv = $("#tabs-transcript")[0];
+    tranStyle = window.getComputedStyle( tranDiv, null);
+    tranBorder = parseInt(tranStyle.getPropertyValue('border-top-width'));
+    tranOffset = tranDiv.offsetTop;
+	$(window).resize();
     $("#tabs-a").tabs("option", "active", 1);
+}
 
-    $(tranDiv).on('dblclick', '#ccTable tbody tr', function () {
-        if ($(this.children[1]).find('span.editable input').length > 0) {
-            $("#jp_player").jPlayer("pause", $("#jp_player").data("jPlayer").status.currentTime);
-            return;
-        }
-        playAudio(parseInt($(this).attr('sTime')));
+function setupWindowScroll() {
+    $(window).resize(function () {
+        document.body.style.overflow = "hidden";
+        tranHeight = $(window).height() - tranOffset - 175;
+		$("#tabs-b").height(tranHeight) 
+		$("#tabs-transcript").height( $("#tabs-b").height() - 55);
+		$("#transcript-content").height($("#tabs-b").height() - 55);
+        document.body.style.overflow = "";
     });
+
+    $('#tabs-transcript').scroll(function () { curElement = $(); });
 }
 
 function renderCaptions(utterances) {
     var index = 0;
 
     $.each(utterances, function (uttIndex, utterance) {
-        var out = '<tr sTime="' + utterance.startTime + '"><td><b>' + formatTimeForDisplay(utterance.startTime) + ':</b></td><td>';
-        $.each(utterance.tokens, function (tokIndex, token) {
+        var out = '<tr><td class="pTime" sTime="' + utterance.startTime + '">' + formatTimeForDisplay(utterance.startTime) + ':</td><td>';
+        $.each( utterance.tokens, function( tokIndex, token ) {
 
             var tokenLabel = normalize(token, tokIndex);
 
             var locId = ("000000" + index).slice(-6);
-            window.loc[index++] = { "startTime": token.startTime, "duration": token.duration, "locId": locId };
-
+            window.loc[ index++] = { "startTime":token.startTime, "duration": token.duration, "locId": locId};
+            
             out += '<span id="' + locId + '"' + getStyles(token) + '>' + tokenLabel + '</span> ';
             renderCorrections(token, tokenLabel);
         });
@@ -91,14 +85,18 @@ function renderCaptions(utterances) {
         ccTable.append(out);
     });
 
-    $("#addNewRSet").click(function (e) {
+    $("#addNewRSet").click(function(e) {
         addRedactionSet();
     });
+
+	$(".pTime").dblclick(function(){
+		playAudio( $(this).attr('sTime'));
+	});
 
     $(".editable").click(function (e) {
         var eid = $(this).attr('id');
         selectText = $(this).text();
-        $("#" + eid).editable("click", function (ie) {
+        $("#" + eid).editable("click", function(ie) {
             if (selectText != ie.value) {
                 var vs = parseInt(eid);
                 var start = window.loc[vs].startTime;
@@ -112,11 +110,12 @@ function renderCaptions(utterances) {
             }
         });
     })
-    .mouseup(function (e) {
+    .mouseup( function (e) {
         if (selectStart != "") selectEnd = $(this).attr('id');
         else selectEnd = "";
     })
-    .mousedown(function (e) {
+    .mousedown(function (e)
+    {
         selectStart = $(this).attr('id');
         selectEnd = "";
     });
@@ -158,35 +157,32 @@ function renderCorrections(token, tokenLabel) {
     }
 }
 
-function renderRedactions_old() {
+function renderRedactions() {
     try {
-        var found = false;
-        $.each(window.redactions, function (index, redaction) {
-            var out = '<tr>';
-            out += '<td>' + formatTimeForDisplay(redaction.startTime) + '</td>';
-            out += '<td>' + formatTimeForDisplay(redaction.endTime) + '</td>';
-            out += '<td>' + redaction.redactionSet + '</td>';
-            out += '<td><a href="#removeRedaction" id="Redact' + index + '" text-align="center"><img alt="remove" src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAYAAAAf8/9hAAACNUlEQVR42rXTy08TURQG8G9maHhYpsDYh4VQUKSmkOBCW0hcFNIZaJEFJMjGjcSV8d8xbkw0bjAxLjQ0mKLSQktbqlao9IHYB9ZFH6GlBSq0zTh1awdNjGdxN/fmd893ci+BfyzivwEzt+ak35PJbpIkUa1WSxvu9dgfganpmaZMKmWi29oM5XJ5YG52dvrw6AiJeCwe2/u2mEmlIwQBm8fl3KkLXDcMj9y9d9/R09sroWkatEyGUukY+fwBjgTosFjEo4cPrCvLtpt1Af3wiG5w6Kq3lZZJKUkDKJL6dULophYDP0oleJxrjwOf/POiMzCOjto0vRdZuVKFxkYJYtE4WqRSFPJ5NDY14UskfEOI4BIFuPFxq5HjLAODQ1ApFPjg96OG7YSCyKTT8HndepfD7hMFWI6zjnETFq1uABpNNz4Hg2jvYBAKBJDNZrG+ate7Vh3iACcARpa1XNbqBECDUCSC8wolglubSGfScK6807uda+KAiWWFDsYtff1X0NnVhVA4AoZhsBMOYT+3j7evl/TvvR5xYMJstprMZoum5xLkwgy2t4UITAeSiQSKhQKWFl+dDVgmJ5/dvjM/x8iVaGluxpaQ/Zy0FTxfRTaTPVl4+sTw0bexWRdQqlR04eBg6prBwKouqDVqdWc7SVHE193d0unpSToc3A7Eo9EFo4kN298sV34DxkxsQy63LyNJSlLbISA8XEK4nOeFBahWKnylUjns69cev3zxnD/zM/1t/QSEyu0RWnv2HgAAAABJRU5ErkJggg=="></a></td>';
-            out += '</tr>';
+        var table = redTable.DataTable();
 
-            redTable.append(out);
-            found = true;
+        $.each(window.redactions, function(index, redaction) {
+            var s = formatTimeForDisplay(redaction.startTime);
+            var e = formatTimeForDisplay(redaction.endTime);
+            var r = redaction.redactionSet;
+            var d = '<a href="#removeRedaction" id="Redact' + index + '" text-align="center">' +
+                    '<img alt="remove" src="https://rawgit.com/Madhangi-FTI/Audio/master/images/trash.png"></a>';
+            table.row.add([s, e, r, d]).draw();
         });
 
-        if (found)
-            $('a[href="#removeRedaction"]').click(function () {
-                var redactId = $(this).attr('id').replace('Redact', '');
-                removeRedaction(window.redactions[redactId]);
-            });
+        $('a[href="#removeRedaction"]').click(function () {
+            var redactId = $(this).attr('id').replace('Redact', '');
+            removeRedaction(window.redactions[redactId]);
+        });
 
     } catch (err) {
         showRedactionError("Rendering Redactions", err.message);
     }
 }
 
-function renderRedactionSets(redactionSets) {
+function renderRedactionSets( redactionSets) {
     try {
-        $.each(redactionSets, function (index, redactionSet) {
+        $.each( redactionSets, function (index, redactionSet) {
             addRedactionSetDropDown(redactionSet);
         });
 
@@ -262,45 +258,18 @@ function handleError(err, funDesc, showAt) {
         showRedactionError("Rendering Redactions", errText);
     }
 }
-function renderRedactions() {
+
+function renderCorrection( startTime, duration, correctedWord) {
     try {
-        var table = redTable.DataTable();
-
-        $.each(window.redactions, function (index, redaction) {
-            var s = formatTimeForDisplay(redaction.startTime);
-            var e = formatTimeForDisplay(redaction.endTime);
-            var r = redaction.redactionSet;
-            var d = '<a href="#removeRedaction" id="Redact' + index + '" text-align="center">' +
-                    '<img alt="remove" src="https://rawgit.com/Madhangi-FTI/Audio/master/images/trash.png"></a>';
-            table.row.add([s, e, r, d]).draw();
-        });
-
-        $('a[href="#removeRedaction"]').click(function () {
-            var redactId = $(this).attr('id').replace('Redact', '');
-            removeRedaction(window.redactions[redactId]);
-        });
-
-    } catch (err) {
-        showRedactionError("Rendering Redactions", err.message);
-    }
-}
-
-function renderCorrection(startTime, duration, correctedWord) {
-    try {
-        var table = fixTable.DataTable();
         var endTime = startTime + duration;
-        //var out = '<tr>';
-        //out += '<td>' + formatTimeForDisplay(startTime) + '</td>';
-        //out += '<td>' + formatTimeForDisplay(endTime) + '</td>';
-        //out += '<td>' + duration.toFixed(2) + '</td>';
-        //out += '<td>' + correctedWord + '</td>';
-        //out += '</tr>';
-        //fixTable.append(out);
-        var s = formatTimeForDisplay(startTime);
-        var e = formatTimeForDisplay(endTime);
-        var r = duration.toFixed(2);
-        var d = correctedWord;
-        table.row.add([s, e, r, d]).draw();
+        var out = '<tr>';
+        out += '<td>' + formatTimeForDisplay(startTime) + '</td>';
+        out += '<td>' + formatTimeForDisplay(endTime) + '</td>';
+        out += '<td>' + duration.toFixed(2) + '</td>';
+        out += '<td>' + correctedWord + '</td>';
+        out += '</tr>';
+        fixTable.append(out);
+
     } catch (err) {
         showRedactionError("Rendering Redactions", err.message);
     }
@@ -312,7 +281,7 @@ function setupJPlayer() {
             var time = event.jPlayer.status.currentTime;
             player.jPlayer('mute', isMuted(time));
             highlightCurrent(time);
-        },
+        }, 
         supplied: "m4a",
         wmode: "window",
         smoothPlayBar: true,
@@ -322,15 +291,15 @@ function setupJPlayer() {
     });
 
     var url = getAudioUrl();
-    player.jPlayer("setMedia", { m4a: url });
+    player.jPlayer("setMedia", {m4a: url});
 }
 
 function playAudio(time) {
-    player.jPlayer('play', time);
+    player.jPlayer('pause');
+    player.jPlayer('play', parseInt(time));
 }
 
-function highlightCurrent(curTime) {
-    tranHeight = $("#tabs-b").height();
+function highlightCurrent( curTime) {
     $.each(window.loc, function (key, loc) {
         if (curTime < loc.startTime) return false;
         if (curTime >= loc.startTime && curTime <= loc.startTime + loc.duration)
@@ -349,7 +318,6 @@ function isMuted(curTime) {
                 muted = true;
                 return false;
             }
-            
             return true;
         });
     }
@@ -369,15 +337,4 @@ function setupRedactionSetButtons() {
 
     $("#rsMenu").hide().menu();
     $("#rsButtons").buttonset();
-}
-
-function setupWindowScroll() {
-    $(window).resize(function () {
-        document.body.style.overflow = "hidden";
-        tranHeight = $(window).height() - tranOffset - 15;
-        $("#transcript-content").height(tranHeight);
-        document.body.style.overflow = "";
-    });
-
-    $('#tabs-transcript').scroll(function () { curElement = $(); });
 }
